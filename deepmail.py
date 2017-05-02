4b1fb9a9b315c0cbca02f7d0b85ca1c70421814f
Siddharth <sgiedne@gmail.com>
2017-05-01 23:33:49 -0500

    Implemented depth-2 search on trained model || April 25 2017 || Sidd

--- deepmail.py
+++ deepmail.py
@@ -4,14 +4,15 @@ import nltk
 import gensim
 from gensim import corpora, models, similarities
 import json
+import unicodedata
 
 sentences = []
 with open('50newstune_articles_text_title.json') as json_data:
     for line in json_data:
         article = {}
         article = json.loads(line)
-        title = nltk.word_tokenize(article['title'].lower())
-        text = nltk.word_tokenize(article['text'].lower())
+        title = nltk.word_tokenize(unicodedata.normalize('NFKD', article['title']).encode('ascii','ignore'))
+        text = nltk.word_tokenize(unicodedata.normalize('NFKD', article['text']).encode('ascii','ignore'))
         sentences.append(title)
         sentences.append(text)
 
@@ -20,37 +21,37 @@ with open('50newstune_articles_text_title.json') as json_data:
 # model = gensim.models.Word2Vec(sentences, size = 200, workers=4)
 
 # model.save('testmodel')
-# model = gensim.models.Word2Vec.load('testmodel')
-# print 'here'
-# for x in model.wv.most_similar(['cat']):
-#     print x
 
-# print model.doesnt_match("Trump Obama Putin tennis".split())
-#model.most_similar([vector])
+input = ['cat','Pooh','bionic']
 
-for x in sentences:
-    if 'cat' in x and 't-shirt' in x:
-        print x
+def get_similar(bow):
+    model = gensim.models.Word2Vec.load('testmodel')
+    return model.wv.most_similar(bow, topn=30)
 
-# for x in sentences:
-#     if 'cat' in x:
-#         if('t-shirt' in x):
-#             print ' '.join(x)
-#             print ''
-#         if('ankle' in x):
-#             print ' '.join(x)
-#             print ''
-#         if('spectator' in x):
-#             print ' '.join(x)
-#             print ''
-#     if 'ankle' in x:
-#         if('t-shirt' in x):
-#             print ' '.join(x)
-#             print ''
-#         if('spectator' in x):
-#             print ' '.join(x)
-#             print ''
-#     if 't-shirt' in x:
-#         if('spectator' in x):
-#             print ' '.join(x)
-#             print ''
+list_depth0 = get_similar(input)
+
+search_result = []
+
+for x in range(0,3):
+    search_result.append([input,list_depth0[x][0]])
+    list_depth1 = get_similar([input,list_depth0[x][0]])
+    search_result.append([list_depth0[x][0], list_depth1[x][0]])
+    search_result.append([input, list_depth1[x][0]])
+    search_result.append([input, list_depth0[x][0], list_depth1[x][0]])
+
+
+res = set()
+for x in search_result:
+    for y in sentences:
+        if len(x) == 3:
+            if x[0] in y and x[1] in y and x[2] in y:
+                res.add(' '.join(y))
+                # print ''
+        else:
+            if x[0] in y and x[1] in y:
+                res.add(' '.join(y))
+                # print ''
+
+for x in res:
+    print x
+    print ''
